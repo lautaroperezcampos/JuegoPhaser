f5f9643d5be25df8242d712fc9b2f3dc60eb241f
Lautaro Perez <lautaroperezcampos@gmail.com>
2026-05-26 22:04:29 -0300

    Mejora 2: Asignar un puntaje distinto a cada elemento recolectable. Condición de GANAR: superar los 100 puntos.

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -2,16 +2,21 @@
 
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
-    // key of the scene
-    // the key will be used to start the scene by other scenes
     super("hello-world");
     this.timeLeft = 30;
     this.gameEnded = false;
+    this.score = 0;
+    this.itemValues = {
+      square: 20,
+      triangle: 15,
+      diamond: 25,
+    };
   }
 
   init() {
     this.timeLeft = 30;
     this.gameEnded = false;
+    this.score = 0;
   }
 
   preload() {
@@ -45,12 +50,91 @@ export default class HelloWorldScene extends Phaser.Scene {
       color: "#ffffff",
     });
 
+    this.scoreText = this.add.text(16, 48, `Puntaje: ${this.score}`, {
+      fontFamily: "Arial",
+      fontSize: "24px",
+      color: "#ffffff",
+    });
+
+    this.items = this.physics.add.group();
+    this.physics.add.overlap(logo, this.items, this.collectItem, null, this);
+
+    this.createItemTextures();
+
+    this.spawnEvent = this.time.addEvent({
+      delay: 1000,
+      callback: this.spawnItem,
+      callbackScope: this,
+      loop: true,
+    });
+
     this.time.addEvent({
       delay: 1000,
       callback: this.tickTimer,
       callbackScope: this,
       loop: true,
     });
+  }
+
+  createItemTextures() {
+    const graphics = this.add.graphics();
+
+    graphics.fillStyle(0x3498db, 1);
+    graphics.fillRect(0, 0, 32, 32);
+    graphics.generateTexture("square", 32, 32);
+    graphics.clear();
+
+    graphics.fillStyle(0xe74c3c, 1);
+    graphics.beginPath();
+    graphics.moveTo(16, 0);
+    graphics.lineTo(32, 32);
+    graphics.lineTo(0, 32);
+    graphics.closePath();
+    graphics.fillPath();
+    graphics.generateTexture("triangle", 32, 32);
+    graphics.clear();
+
+    graphics.fillStyle(0xf1c40f, 1);
+    graphics.beginPath();
+    graphics.moveTo(16, 0);
+    graphics.lineTo(32, 16);
+    graphics.lineTo(16, 32);
+    graphics.lineTo(0, 16);
+    graphics.closePath();
+    graphics.fillPath();
+    graphics.generateTexture("diamond", 32, 32);
+    graphics.destroy();
+  }
+
+  spawnItem() {
+    if (this.gameEnded) {
+      return;
+    }
+
+    const types = ["square", "triangle", "diamond"];
+    const type = Phaser.Math.RND.pick(types);
+    const x = Phaser.Math.Between(40, 760);
+    const item = this.items.create(x, -20, type);
+    item.setVelocity(0, 120);
+    item.setData("type", type);
+    item.setData("value", this.itemValues[type]);
+    item.setCollideWorldBounds(true);
+    item.setBounce(0.1);
+  }
+
+  collectItem(player, item) {
+    if (this.gameEnded) {
+      return;
+    }
+
+    const value = item.getData("value") || 0;
+    this.score += value;
+    item.destroy();
+    this.scoreText.setText(`Puntaje: ${this.score}`);
+
+    if (this.score > 100) {
+      this.endGame(true);
+    }
   }
 
   tickTimer() {
@@ -62,22 +146,35 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.timerText.setText(`Tiempo: ${this.timeLeft}`);
 
     if (this.timeLeft <= 0) {
-      this.endGame();
+      this.endGame(false);
     }
   }
 
-  endGame() {
+  endGame(won = false) {
+    if (this.gameEnded) {
+      return;
+    }
+
     this.gameEnded = true;
+    if (this.spawnEvent) {
+      this.spawnEvent.remove(false);
+    }
+
+    const message = won ? "¡GANASTE!" : "¡PERDISTE!";
+    const detail = won
+      ? "Superaste los 100 puntos."
+      : "Se acabó el tiempo.";
+
     this.add
-      .text(400, 300, "¡PERDISTE!", {
+      .text(400, 300, message, {
         fontFamily: "Arial",
         fontSize: "64px",
-        color: "#ff0000",
+        color: won ? "#2ecc71" : "#ff0000",
       })
       .setOrigin(0.5);
 
     this.add
-      .text(400, 360, "Se acabó el tiempo. Recarga la página para jugar de nuevo.", {
+      .text(400, 360, detail, {
         fontFamily: "Arial",
         fontSize: "24px",
         color: "#ffffff",
